74230cb19effadb2067aa57037fdfc0924f45812
Zinovii Dmytriv <zinovii.dmytriv@verygood.systems>
2018-01-02 13:40:52 +0200

    provisioning

---
+++ provisioning/brew.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+brew install coreutils
+brew install git
+brew install jq
+brew install htop
+brew install kubernetes-cli
+brew install maven
+brew install gradle
+brew install midnight-commander
+brew install node
+brew install pass
+brew install pgcli
+brew install redis
+brew install terminal-notifier
+brew install tmux
+brew install transmission
+brew install tree
+brew install youtube-dl
+brew install zsh
